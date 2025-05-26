4df1266b8eb2790cf824d6eec3d2dadd16b3c5ae
andresfls-buc <andresflsxx@gmail.com>
2025-05-26 19:21:01 +0900

    Initial Commit

---
+++ myApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myApp
+//
+//  Created by Andres Landazabal on 2025/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ myAppTests/myAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  myAppTests.swift
+//  myAppTests
+//
+//  Created by Andres Landazabal on 2025/05/26.
+//
+
+import Testing
+@testable import myApp
+
+struct myAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
